5750d2d05cc3e67ef8371ed5dcd73adc25b81ed1
desig53 <desig53@gmail.com>
2021-05-29 16:42:54 +0800

    Given a binary array nums, return the maximum number of consecutive 1's in the array.

--- leetcode_pracitce/find_max_consecutive_ones.py
+++ leetcode_pracitce/find_max_consecutive_ones.py
@@ -1,5 +1,22 @@
 #Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
+##Example 1:
+
+#Input: nums = [1,1,0,1,1,1]
+#Output: 3
+#Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive
+
+##Example 2:
+##
+##Input: nums = [1,0,1,1,0,1]
+##Output: 2
+
+##Constraints:
+##
+##1 <= nums.length <= 105
+##nums[i] is either 0 or 1.
+
+
 ##找到陣列中，連續數字為1，次數最多的
 
 class Solution(object):
